8523c50c56e6e82451fe80482a6547cad9006cea
Bruno Willenborg <b.willenborg@tum.de>
2022-04-25 18:08:15 +0200

    Update version

--- conf.py
+++ conf.py
@@ -24,8 +24,8 @@ copyright = u'2021, Bruno Willenborg & Hannes Harter'
 author = u'Bruno Willenborg & Hannes Harter'
 
 # The short X.Y version
-version =  '1.0.0'
-release = '1.0.0'
+version =  '1.1.0'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 
